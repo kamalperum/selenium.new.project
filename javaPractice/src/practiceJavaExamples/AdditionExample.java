a9955338a11880b68a5f1a47bc711c4d45e15db5
kamperum <kamperum@CSCO-W-PF3L6ZHF>
2022-11-15 14:50:42 +0530

    initial commit

---
+++ javaPractice/src/practiceJavaExamples/AdditionExample.java
@@ -0,0 +1,19 @@
+package practiceJavaExamples;
+
+public class AdditionExample {
+	
+	int a = 5;
+	int b = 6;
+	
+	public void operationForaddition() {
+		System.out.println(" addition is " + (a+b));
+	}
+
+	public static void main(String[] args) {
+		AdditionExample A = new AdditionExample();
+		A.operationForaddition();
+		
+
+	}
+
+}
