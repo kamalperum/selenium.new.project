e0abda74b111370dacbf8574447e4ca8999aacc9
kamperum <kamperum@CSCO-W-PF3L6ZHF>
2022-11-15 15:13:15 +0530

    2nd commit

---
+++ javaPractice/src/practiceJavaExamples/SubtractionExample.java
@@ -0,0 +1,21 @@
+
+
+package practiceJavaExamples;
+
+public class SubtractionExample {
+	
+	int a = 10;
+	int b = 6;
+	
+	public void operationForSub() {
+		System.out.println(" subtraction is " + (a-b));
+	}
+
+	public static void main(String[] args) {
+		SubtractionExample S = new SubtractionExample();
+		S.operationForSub();
+		
+
+	}
+
+}
